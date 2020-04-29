6c3d9c4b05ecacfc1cbf0d1ef3ad4d4bb9d47eae
Manali Ghare <manalighare19@gmail.com>
2020-04-29 15:55:23 -0400

    First Demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Tweet from "./Tweet";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [users, setUsers] = useState([
+      { name:"John Snow", message:"Winter is Coming" },
+      { name:"Mike Ross", message:"Hey there!!"},
+      { name:"Rachel Green", message:"We were on a break!"},
+      { name:"Monica Geller", message:"I knowwwww.."}
+    ]);
+
+    return (
+    <div className = "app">
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './App.css';
+function Tweet({name, message}){
+    return(
+        <div className = "tweet">
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h4>Number of likes</h4>
+        </div>
+    );
+}
+
+export default Tweet;
